1e3e86e8b41c0c5ce94e317172ff30f8fbe4f48d
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-11 21:01:46 +0200

    Update myscript.js

--- TI11/myscript.js
+++ TI11/myscript.js
@@ -29,13 +29,49 @@ function getCookie(cname){
 	}
     return "";
 }
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("POST", "adauga.php", true);
-  xhttp.send();
+function insertTheRow(jsonObj) {
+	var arr = JSON.parse(jsonObj);
+	
+	var table = document.getElementById("table");
+	var tr = document.createElement("tr");
+	
+	for (var i = 1; i < arr.length; ++i) {
+		var td = document.createElement("td");
+		td.innerHTML = arr[i];
+		tr.appendChild(td);
+	}
+	table.appendChild(tr);
+}
+
+function addFileOnServer() {
+	var fileUpload = document.getElementById("fileToUpload");	
+	var file = fileUpload.files[0];
+	var formData = new FormData();
+	formData.append('fileToUpload', file, file.name);
+	
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		
+		if (this.readyState == 4 && this.status == 200) {
+			insertTheRow(this.responseText);
+		}
+	};
+	
+	xhr.open("POST", "adauga.php", true);
+	xhr.send(formData);
+}
+function deleteFileOnServer(path, row) {
+	
+	var data = new FormData();
+	data.append('path', path);
+	
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			row.parentNode.removeChild(row);
+		}
+	};
+	
+	xhr.open("POST", "sterge.php", true);
+	xhr.send(data);
 }
