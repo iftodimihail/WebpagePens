5b91dca47b00f0bf73d23aca37de3d2b67b606c1
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-15 18:06:33 +0200

    Update modifica.php

--- TI12/api/modifica.php
+++ TI12/api/modifica.php
@@ -1,3 +1,15 @@
 <?php
-	$_SESSION['cos'] =
+	session_start();
+	$file = fopen("../../../res/produse.txt", "r");
+	$json = '';
+	while(!feof($file)){
+		$json.=fgets($file);
+	}
+	$jsonArr = json_decode($json,true);
+	
+	if(empty($_SESSION['cos'])){
+		$_SESSION['cos'] = array();
+	}
+	array_push($_SESSION['cos'], $jsonArr[$_GET['id']]["nume"]);
+	header('Location: http://localhost:5555/TI12/index.php');
 ?>
