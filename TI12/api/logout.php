aa2942a85145acd10717ebe0a71ee1643839d665
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-15 18:05:14 +0200

    Update logout.php

--- TI12/api/logout.php
+++ TI12/api/logout.php
@@ -1,6 +1,8 @@
-<?php
+<?php	
+	session_start();
 	if(isset($_SESSION['utilizator'])){
+		session_unset();
 		session_destroy();
+		header('Location: http://localhost:5555/TI12/index.php');
 	}
-	header('Location: http://localhost:5555/TI12/index.php');
 ?>
