f384ab74fb9b139ce15394f7c05e5ea30e701ca3
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-15 18:03:12 +0200

    The list of items in the shopping cart

---
+++ TI12/cos.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+	if(isset($_SESSION['utilizator'])){
+		echo "<h1>Bun venit, ".$_SESSION['utilizator']."!</h1>";
+?>
+		<form method="post" action="api/logout.php">
+			<input type="submit" name="logoutBtn" value="Logout">
+		</form>
+<?php
+
+		
+		if(isset($_SESSION['cos'])){
+			echo '<table id="productsTable">';
+			foreach($_SESSION['cos'] as $index => $item){
+				echo '<tr id="'.$index.'">';
+				echo '<td>'.$item.'</td>';
+				echo '</tr>';
+			}
+			echo '</table>';
+		}
+	}
+?>
