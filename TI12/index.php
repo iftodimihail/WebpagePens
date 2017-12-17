dd37721c80e3809fd90a95746ab1d1564b605288
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-17 17:28:13 +0200

    Added patterns on user and password inputs

--- TI12/index.php
+++ TI12/index.php
@@ -12,8 +12,8 @@
 <body>
 	<?php if(!isset($_SESSION['utilizator'])){ ?>
 		<form method="POST" action="api/login.php"> 
-			Utilizator: <input type="text" name="utilizator" required>
-			Parola: <input type="text" name="parola" required>
+			Utilizator: <input pattern="[a-zA-z0-9]{3,15}" type="text" name="utilizator" required>
+			Parola: <input pattern="[a-zA-z0-9]{6,15}" type="text" name="parola" required>
 			<input type="submit" name="loginBtn" value="Login">
 		</form>
 	<?php 
