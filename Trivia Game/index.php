f2e8be81a3c58a993339d63a91caf414086cea25
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-23 13:51:41 +0200

    still in work

--- Trivia Game/index.php
+++ Trivia Game/index.php
@@ -3,24 +3,29 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge"> <!-- † -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Smart Trivia</title>
-</head>
-<body onload="loadDoc('api/login.php')">
-	<?php if(!isset($_SESSION['user'])){ ?>
-	<section id="content">
-	</section>
-	<?php 
-		if($_SESSION['error']){
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge"> <!-- † -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Smart Trivia</title>
+	</head>
+	<body <?php if(!(isset($_SESSION['username']) || isset($_SESSION['error']))) echo "onload=loadDoc('api/login.php')";?>>
+		<section id="content">
+		<?php if(isset($_SESSION['username'])){
+			echo "Welcome, ".$_SESSION['username']."!";
+			?>
+			<form action="api/logout.php">
+				<input type="submit" value="logout">
+			</form>
+		<?php 
+		}
+		else{
 			echo $_SESSION['error'];
 			$_SESSION['error']="";
 		}
-	}
-?>
-<script src="js/myscript.js"></script>
-</body>
+		?>
+		</section>
+	<script src="js/myscript.js"></script>
+	</body>
 </html>
 
