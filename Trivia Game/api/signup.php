d910e1f698de77eb7ef9c8bbcb8d4aae715cc11b
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-24 17:27:24 +0200

    refactored it

--- Trivia Game/api/signup.php
+++ Trivia Game/api/signup.php
@@ -5,71 +5,72 @@
 		$data = htmlspecialchars($data);
 		return $data;
 	} 
-
-	if(!isset($_POST['user']) && !isset($_POST['password']) && !isset($_POST['passwordConf'])){?>
-		<form method="POST" action="api/signup.php">
-			Nume utilizator: <input pattern="[a-zA-z0-9]{3,15}" type="text" name="user" required>
-			Parola: <input id="pass" pattern="[a-zA-z0-9]{6,15}" type="text" name="password" required>
-			Confirmă parola: <input id="passConf" pattern="[a-zA-z0-9]{6,15}" type="text" name="passwordConf" required>
-			<input type="submit" name="signUpBtn" value="Înregistrează-te">
-		</form>
-	<?php
-	}	
-		else{
-			if($_POST['password'] !== $_POST['passwordConf']){?>
-				<form method="POST" action="signup.php">
-					Nume utilizator: <input pattern="[a-zA-z0-9]{3,15}" type="text" name="user" required>
-					Parola: <input id="pass" pattern="[a-zA-z0-9]{6,15}" type="text" name="password" required>
-					Confirmă parola: <input id="passConf" pattern="[a-zA-z0-9]{6,15}" type="text" name="passwordConf" required>
+	
+	function formCase($x){
+		switch($x){
+			case 1:
+				echo '<form method="POST" action="api/signup.php">
+					Nume utilizator: <input pattern="[a-zA-z0-9]_{3,15}" type="text" name="user" required>
+					Parola: <input id="pass" pattern="[a-zA-z0-9]{6,15}" type="password" name="password" required>
+					Confirmă parola: <input id="passConf" pattern="[a-zA-z0-9]{6,15}" type="password" name="passwordConf" required>
 					<input type="submit" name="signUpBtn" value="Înregistrează-te">
-				</form>
-			<?php echo "Parolele introduse nu se potrivesc!";
-			}
-			else{			
-				$user = $_POST['user'];
-				$pass = $_POST['password'];
-				$passConf = $_POST['passwordConf'];
-				test_input($user);
-				test_input($pass);
-				test_input($passConf);
-				$validUser = preg_match('([a-zA-z0-9]{3,15})',$user);
-				$validPass = preg_match('([a-zA-z0-9]{6,15})',$pass);
-				$validPassConf = preg_match('([a-zA-z0-9]{6,15})',$passConf);
-				$json = file_get_contents('../res/utilizatori.json');
-				$jsonArr = json_decode($json,true);
-				if($validUser === 1 && $validPass === 1 && $validPassConf === 1){
-					$flag = 1;
-					foreach($jsonArr as $users){
-						if($users["username"]==$user){
-							$flag = 0;
-							break;
-						}
+				</form>';
+				break;
+			case 2: echo '<form method="POST" action="signup.php">
+					Nume utilizator: <input pattern="[a-zA-z0-9]_{3,15}" type="text" name="user" required>
+					Parola: <input id="pass" pattern="[a-zA-z0-9]{6,15}" type="password" name="password" required>
+					Confirmă parola: <input id="passConf" pattern="[a-zA-z0-9]{6,15}" type="password" name="passwordConf" required>
+					<input type="submit" name="signUpBtn" value="Înregistrează-te">
+				</form>';
+				break;
+		}
+	}
+			
+	if(!isset($_POST['user']) && !isset($_POST['password']) && !isset($_POST['passwordConf'])){
+		formCase(1);
+	}	
+	else{
+		if($_POST['password'] !== $_POST['passwordConf']){
+			formCase(2);
+			echo "Parolele introduse nu se potrivesc!";
+		}
+		else{			
+			$user = $_POST['user'];
+			$pass = $_POST['password'];
+			$passConf = $_POST['passwordConf'];
+			test_input($user);
+			test_input($pass);
+			test_input($passConf);
+			$validUser = preg_match('([a-zA-z0-9]{3,15})',$user);
+			$validPass = preg_match('([a-zA-z0-9]{6,15})',$pass);
+			$validPassConf = preg_match('([a-zA-z0-9]{6,15})',$passConf);
+			$json = file_get_contents('../res/utilizatori.json');
+			$jsonArr = json_decode($json,true);
+			if($validUser === 1 && $validPass === 1 && $validPassConf === 1){
+				$flag = 1;
+				foreach($jsonArr as $users){
+					if($users["username"]==$user){
+						$flag = 0;
+						break;
 					}
-					if($flag === 1){	
-							$arrpush['username'] = $user;
-							$arrpush['password'] = $pass;
-							$jsonArr[sizeof($jsonArr)] = array("username" => $arrpush['username'], "password" =>$arrpush['password']);
-							$json = json_encode($jsonArr, JSON_PRETTY_PRINT);
-							if (json_decode($json) != null){
-								$file = fopen("../res/utilizatori.json","w+");
-								fwrite($file, $json);
-								fclose($file);
-							}
-								header('Location: ../index.php');
+				}
+				if($flag === 1){	
+					$arrpush['username'] = $user;
+					$arrpush['password'] = $pass;
+					$jsonArr[sizeof($jsonArr)] = array("username" => $arrpush['username'], "password" =>$arrpush['password']);
+					$json = json_encode($jsonArr, JSON_PRETTY_PRINT);
+					if (json_decode($json) != null){
+						$file = fopen("../res/utilizatori.json","w+");
+						fwrite($file, $json);
+						fclose($file);
 					}
-					else{
-						?>
-							<form method="POST" action="signup.php">
-								Nume utilizator: <input pattern="[a-zA-z0-9]{3,15}" type="text" name="user" required>
-								Parola: <input id="pass" pattern="[a-zA-z0-9]{6,15}" type="text" name="password" required>
-								Confirmă parola: <input id="passConf" pattern="[a-zA-z0-9]{6,15}" type="text" name="passwordConf" required>
-								<input type="submit" name="signUpBtn" value="Înregistrează-te">
-							</form>
-						<?php echo "Numele de utilizator deja există!";
-					}
+						header('Location: ../index.php');
+				}
+				else{
+					formCase(2);
+					echo "Numele de utilizator deja există!";
 				}
 			}
 		}
-	?>
-
-	
+	}
+?>
