bcf8824d370110e843f1c91708e730a29428aabe
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-25 16:07:06 +0200

    Update logout.php

--- Trivia Game/api/logout.php
+++ Trivia Game/api/logout.php
@@ -1,8 +1,6 @@
 <?php	
 	session_start();
-	if(isset($_SESSION['username'])){
-		session_unset();
-		session_destroy();
-		header('Location: ../index.php');
-	}
+	session_unset();
+	session_destroy();
+	header('Location: ../index.php');
 ?>
