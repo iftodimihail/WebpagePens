df292bfd496644328371131a411c21f5a1812a27
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-25 16:08:07 +0200

    Now index is used just for login and sign up

--- Trivia Game/index.php
+++ Trivia Game/index.php
@@ -7,22 +7,13 @@
 		<meta charset="utf-8">
 		<meta http-equiv="x-ua-compatible" content="ie=edge"> <!-- † -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<title>Smart Trivia</title>
 	</head>
 	<body <?php if(!isset($_SESSION['username'])) echo "onload=loadDoc('api/login.php')";?>>
-		<section id="content">
-		<?php if(isset($_SESSION['username'])){
-			echo "Welcome, ".$_SESSION['username']."!";
-			?>
-			<form action="api/logout.php">
-				<input type="submit" value="logout">
-			</form>
-			<?php
-		}
-		?>
-		
-		</section>
-	<script src="js/myscript.js"></script>
+		<main id="content">
+			
+		</main>
+		<script src="js/myscript.js"></script>
 	</body>
 </html>
-
