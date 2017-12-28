ebd657a5ae05d541db0b5f46ebef08cc4b1f0c0d
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2017-12-28 20:03:21 +0200

    Update computers.php

--- Trivia Game/categories/computers.php
+++ Trivia Game/categories/computers.php
@@ -1,21 +1,16 @@
 <?php
 	session_start();
-	if(!isset($_GET['difficulty'])){
+	echo "<h1>".ucfirst($_GET['category'])."</h1>";
 ?>
-		<div class = "difficulty">
-			<div id="easy"><a href="?category=computers&difficulty=easy">
-				Easy
-			</a></div>
-			<div id ="medium"><a href="?category=computers&difficulty=medium">
-				Medium 
-			</a></div>
-			<div id="hard"><a href="?category=computers&difficulty=hard">
-				Hard
-			</a></div>
-		</div>
-<?php
-	}
-	else{
-		echo $_GET['difficulty'];
-	}
-?>
+<input id="category" type="hidden" value=<?php echo $_GET['category']; ?>>
+<div class="quiz-container">
+	<div id="quiz" class="container">
+		<div id="question"></div>
+			<label class="option"><input type="radio" name="button" id="btn1" value="1"><span id="choice1"></span></label>
+			<label class="option"><input type="radio" name="button" id="btn2" value="2"><span id="choice2"></span></label>
+			<label class="option"><input type="radio" name="button" id="btn3" value="3"><span id="choice3"></span></label>
+			<label class="option"><input type="radio" name="button" id="btn4" value="4"><span id="choice4"></span></label>
+			<button id="nextBtn" onclick="loadNextQuestion()">Next Question</button>
+	</div>
+	<div id="result" style="display:none;"></div>
+</div>
